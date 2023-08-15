ff0c323c6817c7c728d1a9b5f2e6f8cddcda0437
Claudio Paladini <clode.paladini@gmail.com>
2023-08-15 14:01:19 +0200

    mobilr

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("header").style.backgroundColor = "rgba(255,255,255,0.4)";
+    document.getElementById("header").style.backgroundColor = "rgba(255,255,255,0.8)";
   } else {
     document.getElementById("header").style.backgroundColor = "rgba(255,255,255,0)";
   }
